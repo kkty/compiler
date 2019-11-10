9a5e538f5c5b695610670faa43b430eb1c24330c
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-11-11 08:00:52 +0900

    add ast package

---
+++ ast/ast.go
@@ -0,0 +1,82 @@
+package ast
+
+type Node interface {
+	astNode()
+}
+
+type Variable struct{ Name string }
+type Unit struct{}
+type Int struct{ Value int32 }
+type Bool struct{ Value bool }
+type Float struct{ Value float32 }
+type Add struct{ Left, Right Node }
+type Sub struct{ Left, Right Node }
+type FloatAdd struct{ Left, Right Node }
+type FloatSub struct{ Left, Right Node }
+type FloatDiv struct{ Left, Right Node }
+type FloatMul struct{ Left, Right Node }
+type Equal struct{ Left, Right Node }
+type LessThanOrEqual struct{ Left, Right Node }
+type Neg struct{ Inner Node }
+type FloatNeg struct{ Inner Node }
+type Not struct{ Inner Node }
+type If struct{ Condition, True, False Node }
+
+type ValueBinding struct {
+	Name       string
+	Body, Next Node
+}
+
+type FunctionBinding struct {
+	Name       string
+	Args       []string
+	Body, Next Node
+}
+
+type Application struct {
+	Function Node
+	Args     []Node
+}
+
+type Tuple struct{ Elements []Node }
+
+type TupleBinding struct {
+	Names       []string
+	Tuple, Next Node
+}
+
+type ArrayCreate struct{ Size, Value Node }
+
+type ArrayGet struct {
+	Array, Index Node
+}
+
+type ArrayPut struct {
+	Array, Index, Value Node
+}
+
+func (n Variable) astNode()        {}
+func (n Unit) astNode()            {}
+func (n Int) astNode()             {}
+func (n Bool) astNode()            {}
+func (n Float) astNode()           {}
+func (n Add) astNode()             {}
+func (n Sub) astNode()             {}
+func (n FloatAdd) astNode()        {}
+func (n FloatSub) astNode()        {}
+func (n FloatDiv) astNode()        {}
+func (n FloatMul) astNode()        {}
+func (n Equal) astNode()           {}
+func (n LessThanOrEqual) astNode() {}
+func (n Neg) astNode()             {}
+func (n FloatNeg) astNode()        {}
+func (n Not) astNode()             {}
+func (n If) astNode()              {}
+func (n ValueBinding) astNode()    {}
+func (n FunctionBinding) astNode() {}
+func (n Application) astNode()     {}
+func (n Tuple) astNode()           {}
+func (n TupleBinding) astNode()    {}
+func (n ArrayCreate) astNode()     {}
+func (n ArrayGet) astNode()        {}
+func (n ArrayPut) astNode()        {}
