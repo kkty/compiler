eb659a1a8a77ffaba35a94dd9053df57421e75ab
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-12-02 11:20:12 +0900

    interpreter: add tests

---
+++ interpreter/interpreter_test.go
@@ -0,0 +1,53 @@
+package interpreter
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/kkty/mincaml-go/ir"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecute(t *testing.T) {
+	for i, c := range []struct {
+		functions     []*ir.Function
+		main          ir.Node
+		input, output string
+	}{
+		{
+			[]*ir.Function{},
+			&ir.ValueBinding{
+				"a", &ir.ReadInt{},
+				&ir.ValueBinding{
+					"b",
+					&ir.AddImmediate{"a", 10},
+					&ir.PrintInt{"b"},
+				},
+			},
+			"1", "11",
+		},
+		{
+			[]*ir.Function{
+				&ir.Function{"f", []string{"a", "b"}, &ir.Sub{"a", "b"}},
+			},
+			&ir.ValueBinding{
+				"x", &ir.ReadInt{},
+				&ir.ValueBinding{
+					"y", &ir.ReadInt{},
+					&ir.ValueBinding{
+						"z", &ir.Application{"f", []string{"x", "y"}},
+						&ir.PrintInt{"z"},
+					},
+				},
+			},
+			"2 3", "-1",
+		},
+	} {
+		t.Run(fmt.Sprintf("Case%d", i), func(t *testing.T) {
+			buf := bytes.Buffer{}
+			Execute(c.functions, c.main, &buf, bytes.NewBufferString(c.input))
+			assert.Equal(t, c.output, buf.String())
+		})
+	}
+}
