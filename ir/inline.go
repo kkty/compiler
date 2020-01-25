cfe5c4c5690bfcbc291098aedb1f6f0dd7da8979
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-01-26 00:40:54 +0900

    ir/inline: refactor

--- ir/inline.go
+++ ir/inline.go
@@ -2,142 +2,130 @@ package ir
 
 import (
 	"fmt"
-	"os"
-	"sort"
-
 	"github.com/kkty/compiler/typing"
+	"os"
 )
 
-var nextTemporaryId = 0
+// Inline does inline expansions for all non-recursive functions and n recursive functions.
+func Inline(main Node, functions []*Function, n int, types map[string]typing.Type, debug bool) (Node, []*Function) {
+	nextTemporaryId := 0
 
-func temporary() string {
-	defer func() { nextTemporaryId++ }()
-	return fmt.Sprintf("_inline_%d", nextTemporaryId)
-}
+	temporary := func() string {
+		defer func() { nextTemporaryId++ }()
+		return fmt.Sprintf("_inline_%d", nextTemporaryId)
+	}
 
-func rename(node Node, types map[string]typing.Type) {
-	valueBindings := []*ValueBinding{}
+	// rename all names in node
+	rename := func(node Node) {
+		valueBindings := []*ValueBinding{}
 
-	// Finds application bindings in the subtree.
+		// find all value bindings using bfs
 
-	queue := []Node{node}
+		queue := []Node{node}
 
-	for len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
+		for len(queue) > 0 {
+			node := queue[0]
+			queue = queue[1:]
 
+			switch node.(type) {
+			case *IfEqual:
+				n := node.(*IfEqual)
+				queue = append(queue, n.True, n.False)
+			case *IfEqualZero:
+				n := node.(*IfEqualZero)
+				queue = append(queue, n.True, n.False)
+			case *IfEqualTrue:
+				n := node.(*IfEqualTrue)
+				queue = append(queue, n.True, n.False)
+			case *IfLessThan:
+				n := node.(*IfLessThan)
+				queue = append(queue, n.True, n.False)
+			case *IfLessThanZero:
+				n := node.(*IfLessThanZero)
+				queue = append(queue, n.True, n.False)
+			case *ValueBinding:
+				n := node.(*ValueBinding)
+				valueBindings = append(valueBindings, n)
+				queue = append(queue, n.Value, n.Next)
+			}
+		}
+
+		mapping := map[string]string{}
+		for _, valueBinding := range valueBindings {
+			t := temporary()
+			types[t] = types[valueBinding.Name]
+			mapping[valueBinding.Name] = t
+		}
+
+		node.UpdateNames(mapping)
+	}
+
+	// replace the applications of the given function with (a copy of) the function body
+	var replaceApplications func(Node, *Function) Node
+	replaceApplications = func(node Node, function *Function) Node {
 		switch node.(type) {
 		case *IfEqual:
 			n := node.(*IfEqual)
-			queue = append(queue, n.True, n.False)
+			n.True = replaceApplications(n.True, function)
+			n.False = replaceApplications(n.False, function)
+			return n
 		case *IfEqualZero:
 			n := node.(*IfEqualZero)
-			queue = append(queue, n.True, n.False)
+			n.True = replaceApplications(n.True, function)
+			n.False = replaceApplications(n.False, function)
+			return n
 		case *IfEqualTrue:
 			n := node.(*IfEqualTrue)
-			queue = append(queue, n.True, n.False)
+			n.True = replaceApplications(n.True, function)
+			n.False = replaceApplications(n.False, function)
+			return n
 		case *IfLessThan:
 			n := node.(*IfLessThan)
-			queue = append(queue, n.True, n.False)
+			n.True = replaceApplications(n.True, function)
+			n.False = replaceApplications(n.False, function)
+			return n
 		case *IfLessThanZero:
 			n := node.(*IfLessThanZero)
-			queue = append(queue, n.True, n.False)
+			n.True = replaceApplications(n.True, function)
+			n.False = replaceApplications(n.False, function)
+			return n
 		case *ValueBinding:
 			n := node.(*ValueBinding)
-			valueBindings = append(valueBindings, n)
-			queue = append(queue, n.Value, n.Next)
-		}
-	}
-
-	mapping := map[string]string{}
-	for _, valueBinding := range valueBindings {
-		t := temporary()
-		types[t] = types[valueBinding.Name]
-		mapping[valueBinding.Name] = t
-	}
-
-	node.UpdateNames(mapping)
-}
-
-// Replaces the applications of the given function with (a copy of) the function body.
-func replaceApplications(node Node, function *Function, types map[string]typing.Type) Node {
-	switch node.(type) {
-	case *IfEqual:
-		n := node.(*IfEqual)
-		n.True = replaceApplications(n.True, function, types)
-		n.False = replaceApplications(n.False, function, types)
-		return n
-	case *IfEqualZero:
-		n := node.(*IfEqualZero)
-		n.True = replaceApplications(n.True, function, types)
-		n.False = replaceApplications(n.False, function, types)
-		return n
-	case *IfEqualTrue:
-		n := node.(*IfEqualTrue)
-		n.True = replaceApplications(n.True, function, types)
-		n.False = replaceApplications(n.False, function, types)
-		return n
-	case *IfLessThan:
-		n := node.(*IfLessThan)
-		n.True = replaceApplications(n.True, function, types)
-		n.False = replaceApplications(n.False, function, types)
-		return n
-	case *IfLessThanZero:
-		n := node.(*IfLessThanZero)
-		n.True = replaceApplications(n.True, function, types)
-		n.False = replaceApplications(n.False, function, types)
-		return n
-	case *ValueBinding:
-		n := node.(*ValueBinding)
-		n.Value = replaceApplications(n.Value, function, types)
-		n.Next = replaceApplications(n.Next, function, types)
-		return n
-	case *Application:
-		n := node.(*Application)
-		if n.Function != function.Name {
+			n.Value = replaceApplications(n.Value, function)
+			n.Next = replaceApplications(n.Next, function)
 			return n
+		case *Application:
+			n := node.(*Application)
+			if n.Function != function.Name {
+				return n
+			}
+			f := function.Body.Clone()
+			rename(f)
+			mapping := map[string]string{}
+			for i, arg := range function.Args {
+				mapping[arg] = n.Args[i]
+			}
+			f.UpdateNames(mapping)
+			return f
+		default:
+			return node
 		}
-		f := function.Body.Clone()
-		rename(f, types)
-		mapping := map[string]string{}
-		for i, arg := range function.Args {
-			mapping[arg] = n.Args[i]
-		}
-		f.UpdateNames(mapping)
-		return f
-	default:
-		return node
 	}
-}
 
-// Inline does inline expansions for n times.
-func Inline(main Node, functions []*Function, n int, types map[string]typing.Type, debug bool) (Node, []*Function) {
-	cnt := map[string]int{}
-	for i := 0; i < n; i++ {
-		if len(functions) == 0 {
-			break
-		}
-
-		sort.Slice(functions, func(i, j int) bool {
-			return len(functions[i].Args)+cnt[functions[i].Name]*10 < len(functions[j].Args)+cnt[functions[j].Name]*10
-		})
-
-		function := functions[0]
-
+	inline := func(function *Function) {
 		if debug {
 			fmt.Fprintf(os.Stderr, "inlining %s\n", function.Name)
 		}
 
-		main = replaceApplications(main, function, types)
+		main = replaceApplications(main, function)
 		for _, f := range functions {
 			if f.Name != function.Name {
-				f.Body = replaceApplications(f.Body, function, types)
+				f.Body = replaceApplications(f.Body, function)
 			}
 		}
 
-		if function.IsRecursive() {
-			cnt[function.Name]++
-		} else {
+		// when a non-recursive function is inlined, it can be removed from the function list.
+		if !function.IsRecursive() {
 			updated := []*Function{}
 			for _, f := range functions {
 				if f.Name != function.Name {
@@ -146,6 +134,46 @@ func Inline(main Node, functions []*Function, n int, types map[string]typing.Typ
 			}
 			functions = updated
 		}
+	}
+
+	// non-recursive functions
+
+	for {
+		updated := false
+		for _, function := range functions {
+			if !function.IsRecursive() {
+				inline(function)
+				updated = true
+			}
+		}
+		if !updated {
+			break
+		}
+	}
+
+	// recursive functions
+
+	cnt := map[string]int{}
+
+	priority := func(f *Function) int {
+		return -(f.Body.Size() + cnt[f.Name]*10)
+	}
+
+	for i := 0; i < n; i++ {
+		if len(functions) == 0 {
+			break
+		}
+
+		// select the function with the highest priority
+		var target *Function
+		for _, function := range functions {
+			if target == nil || priority(function) > priority(target) {
+				target = function
+			}
+		}
+
+		inline(target)
+		cnt[target.Name]++
 	}
 
 	return main, functions
