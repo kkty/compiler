41ebd97f0f70ffb5ebef1535c10043c22a976e1a
Kazushi Kitaya <kazushikitaya@gmail.com>
2020-01-28 11:32:08 +0900

    ir/graph: assign different ids to each node

--- ir/graph.go
+++ ir/graph.go
@@ -4,153 +4,162 @@ import (
 	"fmt"
 	"github.com/emicklei/dot"
 	"io/ioutil"
-	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func GenerateGraph(main Node, functions []*Function) error {
+	nextID := 0
+	newID := func() string {
+		defer func() { nextID++ }()
+		return strconv.Itoa(nextID)
+	}
 	var generate func(Node, *dot.Graph) dot.Node
 	generate = func(node Node, g *dot.Graph) dot.Node {
 		switch node.(type) {
 		case *Variable:
 			n := node.(*Variable)
-			return g.Node(fmt.Sprintf("Variable(%s)", n.Name))
+			return g.Node(newID()).Label(fmt.Sprintf("Variable(%s)", n.Name))
 		case *Unit:
-			return g.Node("Unit")
+			return g.Node(newID()).Label("Unit")
 		case *Int:
-			return g.Node(fmt.Sprintf("Int(%d)", node.(*Int).Value))
+			return g.Node(newID()).Label(fmt.Sprintf("Int(%d)", node.(*Int).Value))
 		case *Bool:
-			return g.Node(fmt.Sprintf("Bool(%v)", node.(*Bool).Value))
+			return g.Node(newID()).Label(fmt.Sprintf("Bool(%v)", node.(*Bool).Value))
 		case *Float:
-			return g.Node(fmt.Sprintf("Float(%v)", node.(*Float).Value))
+			return g.Node(newID()).Label(fmt.Sprintf("Float(%v)", node.(*Float).Value))
 		case *Add:
 			n := node.(*Add)
-			return g.Node(fmt.Sprintf("Add(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("Add(%v, %v)", n.Left, n.Right))
 		case *AddImmediate:
 			n := node.(*AddImmediate)
-			return g.Node(fmt.Sprintf("AddImmediate(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("AddImmediate(%v, %v)", n.Left, n.Right))
 		case *Sub:
 			n := node.(*Sub)
-			return g.Node(fmt.Sprintf("Sub(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("Sub(%v, %v)", n.Left, n.Right))
 		case *SubFromZero:
 			n := node.(*SubFromZero)
-			return g.Node(fmt.Sprintf("SubFromZero(%v)", n.Inner))
+			return g.Node(newID()).Label(fmt.Sprintf("SubFromZero(%v)", n.Inner))
 		case *FloatAdd:
 			n := node.(*FloatAdd)
-			return g.Node(fmt.Sprintf("FloatAdd(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("FloatAdd(%v, %v)", n.Left, n.Right))
 		case *FloatSub:
 			n := node.(*FloatSub)
-			return g.Node(fmt.Sprintf("FloatSub(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("FloatSub(%v, %v)", n.Left, n.Right))
 		case *FloatSubFromZero:
 			n := node.(*FloatSubFromZero)
-			return g.Node(fmt.Sprintf("FloatSubFromZero(%v)", n.Inner))
+			return g.Node(newID()).Label(fmt.Sprintf("FloatSubFromZero(%v)", n.Inner))
 		case *FloatDiv:
 			n := node.(*FloatDiv)
-			return g.Node(fmt.Sprintf("FloatDiv(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("FloatDiv(%v, %v)", n.Left, n.Right))
 		case *FloatMul:
 			n := node.(*FloatMul)
-			return g.Node(fmt.Sprintf("FloatMul(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("FloatMul(%v, %v)", n.Left, n.Right))
 		case *Not:
-			return g.Node(fmt.Sprintf("Not(%v)", node.(*Not).Inner))
+			return g.Node(newID()).Label(fmt.Sprintf("Not(%v)", node.(*Not).Inner))
 		case *Equal:
 			n := node.(*Equal)
-			return g.Node(fmt.Sprintf("Equal(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("Equal(%v, %v)", n.Left, n.Right))
 		case *LessThan:
 			n := node.(*LessThan)
-			return g.Node(fmt.Sprintf("LessThan(%v, %v)", n.Left, n.Right))
+			return g.Node(newID()).Label(fmt.Sprintf("LessThan(%v, %v)", n.Left, n.Right))
 		case *IfEqual:
 			n := node.(*IfEqual)
-			gn := g.Node(fmt.Sprintf("IfEqual(%v, %v)", n.Left, n.Right))
+			gn := g.Node(newID()).Label(fmt.Sprintf("IfEqual(%v, %v)", n.Left, n.Right))
 			gn.Edge(generate(n.True, g), "True")
 			gn.Edge(generate(n.False, g), "False")
 			return gn
 		case *IfEqualZero:
 			n := node.(*IfEqualZero)
-			gn := g.Node(fmt.Sprintf("IfEqualZero(%v)", n.Inner))
+			gn := g.Node(newID()).Label(fmt.Sprintf("IfEqualZero(%v)", n.Inner))
 			gn.Edge(generate(n.True, g), "True")
 			gn.Edge(generate(n.False, g), "False")
 			return gn
 		case *IfEqualTrue:
 			n := node.(*IfEqualTrue)
-			gn := g.Node(fmt.Sprintf("IfEqualTrue(%v)", n.Inner))
+			gn := g.Node(newID()).Label(fmt.Sprintf("IfEqualTrue(%v)", n.Inner))
 			gn.Edge(generate(n.True, g), "True")
 			gn.Edge(generate(n.False, g), "False")
 			return gn
 		case *IfLessThan:
 			n := node.(*IfLessThan)
-			gn := g.Node(fmt.Sprintf("IfLessThan(%v, %v)", n.Left, n.Right))
+			gn := g.Node(newID()).Label(fmt.Sprintf("IfLessThan(%v, %v)", n.Left, n.Right))
 			gn.Edge(generate(n.True, g), "True")
 			gn.Edge(generate(n.False, g), "False")
 			return gn
 		case *IfLessThanZero:
 			n := node.(*IfLessThanZero)
-			gn := g.Node(fmt.Sprintf("IfLessThanZero(%v)", n.Inner))
+			gn := g.Node(newID()).Label(fmt.Sprintf("IfLessThanZero(%v)", n.Inner))
 			gn.Edge(generate(n.True, g), "True")
 			gn.Edge(generate(n.False, g), "False")
 			return gn
 		case *ValueBinding:
 			n := node.(*ValueBinding)
-			gn := g.Node(fmt.Sprintf("ValueBinding(%v)", n.Name))
+			gn := g.Node(newID()).Label(fmt.Sprintf("ValueBinding(%v)", n.Name))
 			gn.Edge(generate(n.Value, g), "Value")
 			gn.Edge(generate(n.Next, g), "Next")
 			return gn
 		case *Application:
 			n := node.(*Application)
-			return g.Node(
+			return g.Node(newID()).Label(
 				fmt.Sprintf("Application(%v, [%v])", n.Function,
 					strings.Join(n.Args, ", ")))
 		case *Tuple:
-			return g.Node(fmt.Sprintf("Tuple([%v])", strings.Join(node.(*Tuple).Elements, ", ")))
+			return g.Node(newID()).Label(fmt.Sprintf("Tuple([%v])", strings.Join(node.(*Tuple).Elements, ", ")))
 		case *TupleGet:
 			n := node.(*TupleGet)
-			return g.Node(fmt.Sprintf("TupleGet(%v, %v)", n.Tuple, n.Index))
+			return g.Node(newID()).Label(fmt.Sprintf("TupleGet(%v, %v)", n.Tuple, n.Index))
 		case *ArrayCreate:
 			n := node.(*ArrayCreate)
-			return g.Node(fmt.Sprintf("ArrayCreate(%v, %v)", n.Value, n.Length))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayCreate(%v, %v)", n.Value, n.Length))
 		case *ArrayCreateImmediate:
 			n := node.(*ArrayCreateImmediate)
-			return g.Node(fmt.Sprintf("ArrayCreateImmediate(%v, %v)", n.Value, n.Length))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayCreateImmediate(%v, %v)", n.Value, n.Length))
 		case *ArrayGet:
 			n := node.(*ArrayGet)
-			return g.Node(fmt.Sprintf("ArrayGet(%v, %v)", n.Array, n.Index))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayGet(%v, %v)", n.Array, n.Index))
 		case *ArrayGetImmediate:
 			n := node.(*ArrayGetImmediate)
-			return g.Node(fmt.Sprintf("ArrayGetImmediate(%v, %v)", n.Array, n.Index))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayGetImmediate(%v, %v)", n.Array, n.Index))
 		case *ArrayPut:
 			n := node.(*ArrayPut)
-			return g.Node(fmt.Sprintf("ArrayPut(%v, %v, %v", n.Array, n.Index, n.Value))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayPut(%v, %v, %v", n.Array, n.Index, n.Value))
 		case *ArrayPutImmediate:
 			n := node.(*ArrayPutImmediate)
-			return g.Node(fmt.Sprintf("ArrayPutImmediate(%v, %v, %v", n.Array, n.Index, n.Value))
+			return g.Node(newID()).Label(fmt.Sprintf("ArrayPutImmediate(%v, %v, %v", n.Array, n.Index, n.Value))
 		case *ReadInt:
-			return g.Node("ReadInt")
+			return g.Node(newID()).Label("ReadInt")
 		case *ReadFloat:
-			return g.Node("ReadFloat")
+			return g.Node(newID()).Label("ReadFloat")
 		case *PrintInt:
-			return g.Node("PrintInt")
+			return g.Node(newID()).Label("PrintInt")
 		case *WriteByte:
-			return g.Node("WriteByte")
+			return g.Node(newID()).Label("WriteByte")
 		case *IntToFloat:
-			return g.Node("IntToFloat")
+			return g.Node(newID()).Label("IntToFloat")
 		case *FloatToInt:
-			return g.Node("FloatToInt")
+			return g.Node(newID()).Label("FloatToInt")
 		case *Sqrt:
-			return g.Node("Sqrt")
+			return g.Node(newID()).Label("Sqrt")
 		}
 
-		log.Fatal("invalid node")
-		return g.Node("")
+		panic("invalid node")
 	}
 
-	if err := os.MkdirAll("graphs", 0644); err != nil {
+	if err := os.MkdirAll("graphs", 0777); err != nil {
 		return err
 	}
-
-	for _, function := range functions {
+	for _, function := range append(functions, &Function{
+		Name: "main",
+		Args: []string{},
+		Body: main,
+	}) {
 		g := dot.NewGraph(dot.Directed)
 		generate(function.Body, g)
-		if err := ioutil.WriteFile(fmt.Sprintf("graphs/%s.dot", function.Name), []byte(g.String()), 0644); err != nil {
+		if err := ioutil.WriteFile(
+			fmt.Sprintf("graphs/%s.dot", function.Name),
+			[]byte(g.String()), 0666); err != nil {
 			return err
 		}
 	}
